1fdf07705f62ae4f3f1da54b370cb948dfb648f1
poiu <ponpottboomk@gmail.com>
2024-12-23 15:40:10 +0700

    Update index.js

--- backend/api-gateway/index.js
+++ backend/api-gateway/index.js
@@ -8,6 +8,16 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use('/graphql', (req, res, next) => {
+  const { operationName } = req.body;
+  if (operationName === 'login' || operationName === 'register') {
+    return next();
+  }
+  verifyJWT(req, res, next);
+});
+
+app.use('/graphql', graphqlRoute);
+
 app.listen(PORT, () => {
     console.log(`🚀 API Gateway running at http://localhost:${PORT}/graphql`);
   });

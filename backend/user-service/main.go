c1cab74d87d026b3c1d95404598b06ca24cda5be
poiu <ponpottboomk@gmail.com>
2024-12-23 20:10:01 +0700

    add api/service endpoint

---
+++ backend/user-service/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	app := fiber.New()
+	log.Fatal(app.Listen(":3001"))
+}

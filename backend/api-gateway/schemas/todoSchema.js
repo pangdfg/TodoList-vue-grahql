eedf0dcc19caa74670a1a7c9944e6f3afa77f708
poiu <ponpottboomk@gmail.com>
2024-12-27 20:06:39 +0700

    Fix toggleTodo and editTodo mutation signatures to correctly define 'checked' as a Boolean type

--- backend/api-gateway/schemas/todoSchema.js
+++ backend/api-gateway/schemas/todoSchema.js
@@ -14,8 +14,8 @@ const todoSchema = `
 
   type Mutation {
     createTodo(title: String!): Todo!
-    toggleTodo(id: ID!, checked!): Todo!
-    editTodo(id: ID!, title: String!, checked!): Todo!
+    toggleTodo(id: ID!, checked: Boolean!): Todo!
+    editTodo(id: ID!, title: String!, checked: Boolean!): Todo!
   }
 `;
 
